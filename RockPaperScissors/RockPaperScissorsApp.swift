9c2ee3670f2d8e2567c1cd7d1bfa08f26b462af1
Peter Fischer <spotted@gmail.com>
2022-03-09 15:42:58 -0500

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Peter Fischer on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
